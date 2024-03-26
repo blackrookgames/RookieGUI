8b41c02668714421e4d61c223e077185bd65c5c7
blackrookgames <blackrookgames@gmail.com>
2024-03-26 19:54:04 -0400

    Add project files.

---
+++ RookieGUI/Properties/RookInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RookieGUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Black Rook Games")]
+[assembly: AssemblyProduct("RookieGUI")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.1808.52737")]
+[assembly: AssemblyFileVersion("1.0.1808.52737")]
+
+
